11fb586f9bb409b46839843e2593775de39f4921
Drew Bradley <williamandrewb@gmail.com>
2021-02-01 16:04:01 -0700

    Complete iterations 1 and 2.

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import Person from '../Person/Person';
+import yearbookData from "../../data/yearbook-data"
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+function Cohort() {
+  const cohortStaff = yearbookData.staff.map(person => <Person key = {person.id} person = {person}/>)
+
+  const cohortStudent = yearbookData.students.map(person => <Person key = {person.id} person = {person}/>)
+
+  return (
+    <div>
+      <header className="cohort-name">Staff</header>
+      <div className="cohort-container">
+          { cohortStaff }
+      </div>
+      <header className="cohort-name">Students</header>
+      <div className="cohort-container">
+          { cohortStudent }
+      </div>
+    </div>
+  )
+  
+}
 
 export default Cohort;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+const Person = (props) => {
+  return(
+    <div className="person">
+      <img src={props.person.photo} />
+      <h1 className="person-name">{props.person.name}</h1>
+      <p className="person-quote">"{props.person.quote}"</p>
+      <p className="person-superlative">{props.person.superlative}</p>
+    </div>
+    )
+}
 
 export default Person;
