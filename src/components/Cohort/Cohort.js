61e500ae414a41d7d9beedc2c9dd67456e976fe1
Drew Bradley <williamandrewb@gmail.com>
2021-02-01 17:52:43 -0700

    Iteration 3

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from '../Person/Person';
+import Student from '../Student/Student'
 import yearbookData from "../../data/yearbook-data"
 import './Cohort.css';
 
-function Cohort() {
-  const cohortStaff = yearbookData.staff.map(person => <Person key = {person.id} person = {person}/>)
+class Cohort extends Component {
+  constructor() {
+    super()
+    this.state = {
 
-  const cohortStudent = yearbookData.students.map(person => <Person key = {person.id} person = {person}/>)
+    }
+  }
 
-  return (
-    <div>
-      <header className="cohort-name">Staff</header>
-      <div className="cohort-container">
-          { cohortStaff }
+  handleClick(event) {
+    // const {name, value} = event.target
+    console.log("I'm a student!")
+    // this.setState({[name]: value})
+  }
+
+  render() {
+    const cohortStaff = yearbookData.staff.map(person => <Person key = {person.id} person = {person}/>)
+
+    const cohortStudent = yearbookData.students.map(person => <Student key = {person.id} person = {person}/>)
+
+    return (
+      <div>
+        <header className="cohort-name">Staff</header>
+        <div className="cohort-container">
+            { cohortStaff }
+        </div>
+        <header className="cohort-name">Students</header>
+        <div className="cohort-container">
+            { cohortStudent }
+        </div>
       </div>
-      <header className="cohort-name">Students</header>
-      <div className="cohort-container">
-          { cohortStudent }
-      </div>
-    </div>
-  )
+    )
+  }
   
 }
 
---
+++ src/components/Student/Student.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react"
+import '../Person/Person.css';
+
+class Student extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: props.person.id,
+      name: props.person.name,
+      quote: "",
+      superlative: "",
+      photo: "https://placekitten.com/200/300",
+      hidden: {
+        display: "none"
+      }
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    if(this.state.hidden){
+      this.setState(
+        {hidden: null})
+    }else{
+      this.setState(
+        {hidden: {display: "none"}})
+    }
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return(
+      <div className="person">
+        <img src={this.state.photo} />
+        <h1 className="person-name">{this.state.name}
+        </h1>        
+        <p className="person-quote">{this.state.quote}</p>
+        <p className="person-superlative">{this.state.superlative}</p>
+        <form 
+          style={this.state.hidden}
+          id={this.state.id}>
+          <input 
+            type="text"
+            id={this.state.id}
+            onChange={this.handleChange} 
+            name="name"
+            placeholder="Change name"
+          />
+          <input 
+            type="text"
+            id={this.state.id}
+            onChange={this.handleChange} 
+            name="quote"
+            placeholder="Change quote"
+          />
+          <input 
+            type="text"
+            id={this.state.id}
+            onChange={this.handleChange} 
+            name="superlative"
+            placeholder="Change superlative"
+          />
+        </form>
+          <button onClick={this.handleClick}>Change</button>
+      </div>
+      )
+  }
+}
+
+export default Student;
